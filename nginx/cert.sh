4628532bc3b8891146fa4513442435894f79ccbd
Weaxs <459312872@qq.com>
2020-12-31 14:42:44 +0800

    Create cert.sh

---
+++ nginx/cert.sh
@@ -0,0 +1,6 @@
+export from_path="/etc/letsencrypt/live/example.com"
+export to_path="/home/centos/docker_compose/nginx/certs"
+
+cp $from_path/fullchain.pem $to_path/fullchain.pem
+cp $from_path/privkey.pem $to_path/privkey.pem
+cp $from_path/cert.pem $to_path/cert.pem
