2c1f015704166f9e97cad640b736eaa33eba6386
Weaxs <459312872@qq.com>
2024-06-28 11:22:27 +0800

    support v2fly v5

---
+++ v2fly-v5/nginx/cert.sh
@@ -0,0 +1,7 @@
+certbot certonly --standalone -d example.com -d trojan.example.com
+export from_path="/etc/letsencrypt/live/example.com"
+export to_path="/home/centos/docker_compose/nginx/certs"
+
+cp $from_path/fullchain.pem $to_path/fullchain.pem
+cp $from_path/privkey.pem $to_path/privkey.pem
+cp $from_path/cert.pem $to_path/cert.pem
