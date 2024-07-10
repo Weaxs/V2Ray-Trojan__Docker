c22ed4a1f9976aed3c0755a19099e5d5f9fef00c
Weaxs <459312872@qq.com>
2024-07-10 13:06:27 +0800

    fix v2fly4 nginx.conf

--- cert.sh
+++ cert.sh
@@ -6,7 +6,14 @@ echo "$curr_dir"
 certbot certonly --standalone -d trojan.example.com -d vmess.example.com -d shadowsocks.example.com -d vless.weaxsey.org
 from_path="/etc/letsencrypt/live/trojan.example.com"
 cp "$from_path"/fullchain.pem "$from_path"/fullchain.crt
+cp "$from_path"/privkey.pem "$from_path"/privkey.key
 
-cp -PR "$from_path" "$curr_dir"/v2fly4.vless-vmess-trojan/nginx/certs
-cp -PR "$from_path" "$curr_dir"/v2fly4.vless-vmess-trojan-shadowsocks/nginx/certs
-cp -PR "$from_path" "$curr_dir"/v2fly5.vmess-trojan-shadowsocks/nginx/certs
+target_dirs=(
+  "$curr_dir"/v2fly4.vless-vmess-trojan/nginx/certs
+  "$curr_dir"/v2fly4.vless-vmess-trojan-shadowsocks/nginx/certs
+  "$curr_dir"/v2fly5.vmess-trojan-shadowsocks/nginx/certs
+)
+
+for target in "${target_dirs[@]}"; do
+  find "$from_path" -maxdepth 1 -print0 | xargs -0 -I {} cp -rLf {} "$target"
+done
